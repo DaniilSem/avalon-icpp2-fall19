87d93898927885bf0293860c68194d5995724fd0
DaniilSem <32225017+DaniilSem@users.noreply.github.com>
2019-09-22 13:20:56 +0300

    Add files via upload

--- Homework01.cpp
+++ Homework01.cpp
@@ -24,17 +24,65 @@ bool isbn13(long long int num)
 	res = (10 - (sum % 10) == cont);
 	return res;
 }
+bool isbn10(long long num)
+{
+	int sum = 0;
+	bool res;
+	int cont = num % 10;
+	num = num / 10;
+	for (int i = 2; i < 11; ++i)
+	{
+		sum = sum + (num % 10)*i;
+		num = num / 10;
+	}
+	int n;
+	n = sum % 11;
+	n = 11 - n;
+	res = ((11-(sum%11))==cont);
+	return res;
+}
+//int main()
+//{
+//	int col;
+//	long long		num;
+//	std::cout << "Col of numbers" << std::endl;
+//	std::cin >> col;
+//	for (int i = 0; i < col; ++i)
+//	{
+//		std::cout << "Number" << std::endl;
+//		std::cin >> num;
+//		if (isbn13(num))
+//		{
+//			std::cout << "valid" << std::endl;
+//		}
+//		else
+//		{
+//			std::cout << "invalid" << std::endl;
+//		}
+//	}
+//}
 int main()
 {
-	int col;
-	long long		num;
-	std::cout << "Col of numbers" << std::endl;
-	std::cin >> col;
-	for (int i = 0; i < col; ++i)
+	long long int num = 0;
+	std::cout << "idbn13 or isbn10" << std::endl;
+	int chek;
+	std::cin >> chek;
+	std::cout << "Number" << std::endl;
+	std::cin >> num;
+	if (chek == 13)
 	{
-		std::cout << "Number" << std::endl;
-		std::cin >> num;
 		if (isbn13(num))
+		{
+			std::cout << "valid" << std::endl;
+		}
+		else
+		{
+			std::cout << "invalid" << std::endl;
+		}
+	}
+	else
+	{
+		if (isbn10(num))
 		{
 			std::cout << "valid" << std::endl;
 		}
