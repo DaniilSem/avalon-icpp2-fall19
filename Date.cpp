7c1f4046876902ede1fb4490bc72dac6acb202ba
DaniilSem <32225017+DaniilSem@users.noreply.github.com>
2019-10-25 21:34:06 +0300

    Add files via upload

--- Date.cpp
+++ Date.cpp
@@ -1,8 +1,7 @@
-#include"Date.hpp"
+Ôªø#include"Date.hpp"
 #include<iostream>
 #include<string>
-namespace ext
-{
+namespace ext {
 	TimeDelta countJND(Date date)
 	{
 		unsigned int a, y, m;
@@ -63,7 +62,7 @@ namespace ext
 		}
 		return  "Invalid type of month";
 	}
-	
+
 	void print(Date data, DateFormat format)
 	{
 		std::cout << data.year << " ";
@@ -90,7 +89,7 @@ namespace ext
 	{
 		std::cout << delta.delta << std::endl;
 	}
-	
+
 	Season getSeason(Date date)
 	{
 		return getSeason(date.month);
@@ -102,21 +101,21 @@ namespace ext
 		{
 			return Season::Winter;
 		}
-			if ((static_cast<int>(month) > 2) & (static_cast<int>(month) < 6))
-			{
-				return Season::Spring;
-			}
-			if ((static_cast<int>(month) > 5) & (static_cast<int>(month) < 9))
-			{
-				return Season::Summer;
-			}
-			if ((static_cast<int>(month) > 8) & (static_cast<int>(month) < 12))
-			{
-				return Season::Autumn;
-			}
-			
+		if ((static_cast<int>(month) > 2) & (static_cast<int>(month) < 6))
+		{
+			return Season::Spring;
 		}
-	
+		if ((static_cast<int>(month) > 5) & (static_cast<int>(month) < 9))
+		{
+			return Season::Summer;
+		}
+		if ((static_cast<int>(month) > 8) & (static_cast<int>(month) < 12))
+		{
+			return Season::Autumn;
+		}
+
+	}
+
 	bool  operator == (const Date lhs, const Date rhs)
 	{
 		return lhs.day == rhs.day
@@ -147,14 +146,29 @@ namespace ext
 	{
 		return (lhs == rhs) || (lhs > rhs);
 	}
-	
-//ƒÓÏ‡¯ÌÂÂ Á‡‰‡ÌËÂ 4
-	TimeDelta& operator ++ ( TimeDelta& delta)
+
+	//√Ñ√Æ√¨√†√∏√≠√•√• √ß√†√§√†√≠√®√• 4
+	Date ReJND(TimeDelta time)
+	{
+		int a, b, c, d, e, m;
+		a = time.delta + 32044;
+		b = (4 * a + 3) / 146097;
+		c = a - (146097 * b);
+		d = (4 * c + 3) / 1461;
+		e = c - ((1461 * d) / 4);
+		m = (5 * e + 2) / 153;
+		Date data;
+		data.day = e - ((153 * m + 2) / 2) + 1;
+		data.month = static_cast<Month>(m + 3 - 12 * m / 10);
+		data.year = 100 * b + d - 4800 + m / 10;
+		return data;
+	}
+	TimeDelta& operator ++ (TimeDelta& delta)
 	{
 		++delta.delta;
 		return delta;
 	}
-	TimeDelta& operator ++ (TimeDelta& delta,int)
+	TimeDelta& operator ++ (TimeDelta& delta, int)
 	{
 		TimeDelta temp = delta;
 		++delta.delta;
@@ -167,7 +181,7 @@ namespace ext
 	}
 	TimeDelta& operator -- (TimeDelta& delta, int)
 	{
-		TimeDelta temp=delta;
+		TimeDelta temp = delta;
 		--delta.delta;
 		return temp;
 	}
@@ -176,16 +190,16 @@ namespace ext
 	{
 		++delta.day;
 		++delta.year;
-		delta.month=static_cast<Month>(static_cast<int>(delta.month) + 1);
+		delta.month = static_cast<Month>(static_cast<int>(delta.month) + 1);
 		return delta;
 	}
 	Date& operator ++ (Date& delta, int)
 	{
 		Date temp = delta;
-			++delta.day;
-			++delta.year;
-			delta.month = static_cast<Month>(static_cast<int>(delta.month) + 1);
-			return temp;
+		++delta.day;
+		++delta.year;
+		delta.month = static_cast<Month>(static_cast<int>(delta.month) + 1);
+		return temp;
 	}
 	Date& operator -- (Date& delta)
 	{
@@ -198,8 +212,8 @@ namespace ext
 	{
 		Date temp = delta;
 		--delta.day;
-		-delta.year;
-		delta.month = static_cast<Month>(static_cast<int>(delta.month)- 1);
+		--delta.year;
+		delta.month = static_cast<Month>(static_cast<int>(delta.month) - 1);
 		return temp;
 	}
 
@@ -229,7 +243,7 @@ namespace ext
 
 	Date& getMinDate(Date dates[], int size)
 	{
-		Date min=dates[0];
+		Date min = dates[0];
 
 		for (int i = 1; i < size; ++i)
 		{
@@ -253,9 +267,9 @@ namespace ext
 		}
 		return max;
 	}
-	void sort(Date dates[],int size, SortBy sortBy)
+	void sort(Date dates[], int size, SortBy sortBy)
 	{
-		if(sortBy==SortBy::Date)
+		if (sortBy == SortBy::Date)
 		{
 			bool isSwap = false;
 			for (int i = 0; i < size; ++i)
@@ -263,7 +277,7 @@ namespace ext
 
 				for (int j = 0; j < size - 1 - i; ++j)
 				{
-					if (countJND(j).delta>countJND(j+1).delta)
+					if (countJND(dates[j]).delta > countJND(dates[j + 1]).delta)
 					{
 						Date temp = dates[j];
 						dates[j] = dates[j + 1];
@@ -275,5 +289,86 @@ namespace ext
 				isSwap = false;
 			}
 		}
+		else if (sortBy == SortBy::Season)
+		{
+			bool isSwap = false;
+			for (int i = 0; i < size; ++i)
+			{
 
+				for (int j = 0; j < size - 1 - i; ++j)
+				{
+					if (getSeason(dates[j].month) > getSeason(dates[j + 1].month))
+					{
+						Date temp = dates[j];
+						dates[j] = dates[j + 1];
+						dates[j + 1] = temp;
+						isSwap = true;
+					}
+				}
+				if (!isSwap)return;
+				isSwap = false;
+			}
+		}
+	}
+	bool Leap(int year)
+	{
+		bool leap;
+		if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+		{
+			leap = true;
+		}
+		else
+		{
+			leap = false;
+		}
+		return leap;
+	}
+	int DayMax(bool leap,int month)
+	{
+		int daymax;
+		int monthn = month % 2;
+		switch (monthn)
+		{
+		case 1:
+			daymax = 31;
+			if ((month == 9) || (month == 11))
+			{
+				daymax = 30;
+			}
+			break;
+		case 0:
+			daymax = 30;
+			if ((month == 8) || (month == 10) || (month == 12))
+			{
+				daymax = 31;
+			}
+			if (month == 2)
+			{
+				daymax = 28;
+			}
+			break;
+		}
+		if ((leap) && (month == 2))
+		{
+			daymax = 29;
+		}
+	}
+	Date* tryFillDate(int year, int month, int day)
+	{
+		Date data;
+			int  daymax, monthn;
+			bool leap = Leap(year);
+			daymax = DayMax(leap,month);
+			if ((day < daymax)& (month <= 12))
+			{
+				data.day = day;
+				data.year = year;
+				data.month = static_cast<Month>(month);
+			}
+			else
+			{
+				throw std::exception("Invalid date");
+			}
+			return data*;
+	}
 }
